f3239421638ef8a9d8b6c6b143993f6add9fccd7
Frank DelPidio <frankdaedalus@gmail.com>
2015-07-04 12:46:49 -0400

    create simple csv parser

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,2 +1,10 @@
 puts "EvenManager initialized"
 
+lines = File.readlines "event_attendees.csv"
+
+lines.each_with_index do |line, index|
+  next if index == 0
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end
